a4d3e7d0ae161f361253b0ab9754d0dc512957b8
ks.shewale <ks.shewale@tcs.com>
2026-06-24 13:11:13 +0100

    Commit-variable-changes

--- variable.tf
+++ variable.tf
@@ -1,3 +1,13 @@
-variable "https" {}
-variable "http" {}
-variable "API" {}
+variable "https" {
+    default = 443
+}
+variable "http" {
+    default = 8080
+}
+variable "API" {
+
+    default = 8383
+}
+variable "cider" {
+    default = "172.31.0.0"
+}
